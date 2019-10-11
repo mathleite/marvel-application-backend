8da302364534c90c2d22b3ee70a4e09464cf9a2e
Adriel da Silva <silvaadriel510@gmail.com>
2019-10-11 15:30:56 -0300

    Register guzzle http service

--- integrator/app/Providers/AppServiceProvider.php
+++ integrator/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('GuzzleHttp\Client', function () {
+            return new Client([
+                'base_uri' => 'https://gateway.marvel.com/v1/public/',
+                'query' => [
+                    'apikey' => '6ad1545abd9cd65a8f1203f9b021da32',
+                    'hash' => '1ce381de35ae921ab56fc4d79602fc5d',
+                    'ts' => '1'
+                ]
+            ]);
+        });
     }
 
     /**
